6591f9c3568df30bfd3334a87daa28439da84903
drake.pham <drake.pham@icetea.io>
2024-09-09 09:52:32 +0700

    Init

---
+++ LOKI Network/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LOKI_Network.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
